1398768f078c2d97ae6e55b77194133ef6266b27
SenhaiQu <qusenhai2@gmail.com>
2022-05-13 14:55:24 -0400

    Rest Project

---
+++ rest/src/main/java/com/Senhai/app/rest/Controller/ApiControllers.java
@@ -0,0 +1,54 @@
+package com.Senhai.app.rest.Controller;
+
+import com.Senhai.app.rest.Models.User;
+import com.Senhai.app.rest.Repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ApiControllers {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/")
+    public String getPage() {
+        return "Welcome";
+    }
+
+    @GetMapping(value="/users")
+    public List<User> getUsers(){
+        return userRepo.findAll();
+    }
+
+    @PostMapping(value="/save")
+    public String saveUser(User user){
+        userRepo.save(user);
+        return "saved...";
+    }
+
+
+    @PutMapping(value="/update/{id}")
+    public String updateUser(@PathVariable long id, @RequestBody User user) {
+        User updateUser = userRepo.findById(id).get();
+        updateUser.setFrirstName(user.getFrirstName());
+        updateUser.setLastName(user.getLastName());
+        updateUser.setOccupation(user.getOccupation());
+        updateUser.setAge(user.getAge());
+        return "Updated...";
+    }
+
+    @DeleteMapping(value="/delete/{id}")
+    public String deleteUser(@PathVariable long id){
+        User deleteUser= userRepo.findById(id).get();
+        userRepo.delete(deleteUser);
+        return "Delete user with the id: "+id;
+    }
+
+
+
+
+
+}
---
+++ rest/src/test/java/com/Senhai/app/rest/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Senhai.app.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
